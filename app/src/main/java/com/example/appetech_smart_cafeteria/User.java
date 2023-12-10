b4de1f69ee017136b1d095222c34137cb4b59cc6
kahwai0227 <kahwai0227@gmail.com>
2023-12-10 22:26:19 +0800

    12/10/2023 add new parameters to user class

--- app/src/main/java/com/example/appetech_smart_cafeteria/User.java
+++ app/src/main/java/com/example/appetech_smart_cafeteria/User.java
@@ -2,6 +2,8 @@ package com.example.appetech_smart_cafeteria;
 
 public class User {
     public String username;
+    public String id;
+    public String role;
     public String email;
     public String password;
 
@@ -9,8 +11,10 @@ public class User {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String id, String role, String email, String password) {
         this.username = username;
+        this.id = id;
+        this.role = role;
         this.email = email;
         this.password = password;
     }
@@ -18,8 +22,9 @@ public class User {
     public String getUsername() {
         return username;
     }
+    public String getId() { return id; }
+    public String getRole() { return role; }
     public String getEmail() {return email; }
-
     public String getPassword() {
         return password;
     }
